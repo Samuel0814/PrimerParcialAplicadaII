3e96c3d9e83f7c17bdfe662277a04131f6799e16
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2019-02-01 22:09:38 -0400

    Terminados de hacer parte del contexto y la bll. Ademas de Instalarle EntityFramework a todos los proyecto menos a entidades

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class Class1
+    public class Contexto : DbContext
     {
+        
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
     }
 }
