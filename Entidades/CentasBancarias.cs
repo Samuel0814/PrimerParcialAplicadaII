b5d08643d4858fa0bdfc4d32ecb3476267cfc2f4
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2019-02-01 22:33:27 -0400

    Entidades Completas

---
+++ Entidades/CentasBancarias.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class CuentasBancarias
+    {
+        [Key]
+        public int CuentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public double Balance { get; set; }
+
+        public CuentasBancarias(int cuentaId, DateTime fecha, string nombre)
+        {
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Nombre = nombre;
+        }
+
+        public CuentasBancarias()
+        {
+            CuentaId = 0;
+            Fecha = DateTime.Now;
+            Nombre = String.Empty;
+            Balance = 0;
+        }
+    }
+}
---
+++ Entidades/Depositos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Depositos
+    {
+        [Key]
+        public int DepositosId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CuentaId { get; set; }
+        public string Concepto { get; set; }
+        public double Monto { get; set; }
+        public virtual CuentasBancarias Cuenta { get; set; }
+
+        public Depositos(int depositosId, DateTime fecha, int cuentaId, string concepto, double monto)
+        {
+            DepositosId = depositosId;
+            Fecha = fecha;
+            CuentaId = cuentaId;
+            Concepto = concepto;
+            Monto = monto;
+        }
+
+        public Depositos()
+        {
+            DepositosId = 0;
+            Fecha = DateTime.Now;
+            CuentaId = 0;
+            Concepto = String.Empty;
+            Monto = 0;
+        }
+    }
+}
